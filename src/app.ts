a6bb8f09ef4dc4a35d6a4cb2c8b2da13f1c864f0
Zidane Innis <zidaneinnis@gmail.com>
2023-03-27 07:02:25 -0500

    Intersection Types

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,23 @@
-// Code goes here!
+type Admin = {
+  name: string;
+  privilages: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployees = Admin & Employee;
+
+const e1: ElevatedEmployees = {
+  name: "Zid",
+  privilages: ["create-server"],
+  startDate: new Date(),
+};
+
+type Combinable = string | boolean;
+
+type Numeric = number | boolean;
+
+type Universal = Combinable | Numeric;
