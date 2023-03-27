d00cb25e3c210ab883bd8efbf4d63aa41a2e5435
Zidane Innis <zidaneinnis@gmail.com>
2023-03-27 07:47:56 -0500

    More on Type Guards

--- src/app.ts
+++ src/app.ts
@@ -16,8 +16,60 @@ const e1: ElevatedEmployees = {
   startDate: new Date(),
 };
 
-type Combinable = string | boolean;
+type Combinable = string | number;
 
 type Numeric = number | boolean;
 
 type Universal = Combinable | Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeinformation(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+  if ("privilages" in emp) {
+    console.log("Privilages: " + emp.privilages);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date: " + emp.startDate);
+  }
+}
+
+printEmployeeinformation({ name: "Name", startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+
+  loadCarego(amount: number) {
+    console.log("Loading Cargo..." + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCarego(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
